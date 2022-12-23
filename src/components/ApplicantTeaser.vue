3a0a45c78c3293d84823f6f8125e9c60bec7b8d4
Julian Toscani <julian.toscani@rtl.com>
2022-12-23 16:55:33 +0100

    💄 add applicant teaser

---
+++ src/components/ApplicantTeaser.vue
@@ -0,0 +1,46 @@
+<template>
+  <li>
+    <img :src="applicant.image" :alt="altText" />
+    <div class="applicant-info">
+      <div class="applicant-name">{{ applicant.fullName }}</div>
+      <div class="applicant-job">{{ applicant.jobDescription }}</div>
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  applicant: { fullName: string; image: string; jobDescription: string };
+}>();
+
+const altText = computed(
+  () => `Image of applicant: ${props.applicant.fullName}`
+);
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  gap: 1rem;
+}
+
+.applicant-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.applicant-name,
+.applicant-job {
+  line-height: 1.5;
+}
+
+img {
+  max-height: 4rem;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+  object-position: center;
+}
+</style>
