e7c885800798d3bf6796356bba3bd8a5ce3b6f66
Julian Toscani <julian.toscani@rtl.com>
2022-12-25 18:11:46 +0100

    💄 add application display

--- src/components/ApplicantDisplay.vue
+++ src/components/ApplicantDisplay.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
-    <img :src="props.applicant.image" alt="" />
-    <div>{{ fullName }}</div>
+    <img :src="props.applicant.image" width="320" height="240" />
+    <h2>{{ fullName }}</h2>
     <ul>
+      <li>
+        <School />
+        {{ applicant.university }}
+      </li>
       <li>
         <Mail />
         {{ applicant.email }}
       </li>
-      <li></li>
-      <li></li>
       <li>
-
+        <Work />
+        {{ applicant.company.name }}
+      </li>
+      <li>
+        <Person />
+        {{ applicant.company.title }}
       </li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { ApiApplicant } from "@/types/ApiApplicant.interface";
-
-import Mail from "@material-design-icons/svg/filled/contact_mail.svg?component";
-
+/// <reference types="vite-svg-loader" />
 import { computed } from "vue";
+
+import type { ApiApplicant } from "@/types/ApiApplicant.interface";
+import Mail from "@material-design-icons/svg/filled/contact_mail.svg?component";
+import School from "@material-design-icons/svg/filled/school.svg?component";
+import Work from "@material-design-icons/svg/filled/work.svg?component";
+import Person from "@material-design-icons/svg/filled/person.svg?component";
 
 const props = defineProps<{ applicant: ApiApplicant }>();
 
@@ -29,3 +39,31 @@ const fullName = computed(
   () => `${props.applicant.firstName} ${props.applicant.lastName}`
 );
 </script>
+
+<style scoped>
+h2 {
+  text-align: center;
+  margin-bottom: 0;
+}
+
+img {
+  aspect-ratio: 4/3;
+  object-fit: contain;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 0.5rem;
+}
+
+div {
+  margin: 0 auto;
+  display: flex;
+  overflow: auto;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid black;
+}
+</style>
--- src/views/DecisionView.vue
+++ src/views/DecisionView.vue
@@ -1,5 +1,69 @@
 <template>
-  <h2>Applications</h2>
+  <div class="decision-container">
+    <ApplicantDisplay v-if="applicant" :applicant="applicant" />
+    <div class="button-container">
+      <button>
+        <Done class="done" />
+        <small> Job Interview </small>
+      </button>
+      <button>
+        <Close class="close" />
+        <small> Maybe Later </small>
+      </button>
+    </div>
+  </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped></style>
+<script setup lang="ts">
+/// <reference types="vite-svg-loader" />
+import { onMounted, ref } from "vue";
+import ApplicantDisplay from "@/components/ApplicantDisplay.vue";
+import Done from "@material-design-icons/svg/filled/done.svg?component";
+import Close from "@material-design-icons/svg/outlined/close.svg?component";
+
+import fetchApplicant from "@/lib/fetchApplicant";
+const applicant = ref(null);
+
+onMounted(async () => {
+  try {
+    applicant.value = await fetchApplicant(1);
+  } catch (error) {
+    console.log(error);
+  }
+});
+</script>
+<style scoped>
+.decision-container {
+  display: grid;
+  grid-template-rows: 1fr 6rem;
+}
+
+.button-container {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.done {
+  stroke-width: 4px;
+  stroke: green;
+}
+.close {
+  stroke-width: 4px;
+  stroke: red;
+}
+
+small {
+  white-space: nowrap;
+}
+
+button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  height: 4rem;
+  width: 4rem;
+  background-color: transparent;
+  border: none;
+}
+</style>
