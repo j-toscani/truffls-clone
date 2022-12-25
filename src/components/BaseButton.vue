d1fe6fe10efa24526d92d4531fa515ebf7723182
Julian Toscani <julian_toscani@gmx.de>
2022-12-25 20:33:25 +0100

    add button component, fix bug

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+button {
+  cursor: pointer;
+  transition: background-color 0.2s ease-out;
+}
+
+@media (hover: hover) {
+  button:hover {
+    background-color: rgba(220,220,220);
+  }
+}
+
+button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0.5rem;
+  gap: 0.5rem;
+  background-color: transparent;
+  border: none;
+}
+</style>
