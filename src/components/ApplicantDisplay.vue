4de3dc862e311d45d80470b982e8c6046be4b2b4
Julian Toscani <julian.toscani@rtl.com>
2022-12-25 17:04:09 +0100

    WIP: 2hr breakpoint

---
+++ src/components/ApplicantDisplay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img :src="props.applicant.image" alt="" />
+    <div>{{ fullName }}</div>
+    <ul>
+      <li>
+        <Mail />
+        {{ applicant.email }}
+      </li>
+      <li></li>
+      <li></li>
+      <li>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ApiApplicant } from "@/types/ApiApplicant.interface";
+
+import Mail from "@material-design-icons/svg/filled/contact_mail.svg?component";
+
+import { computed } from "vue";
+
+const props = defineProps<{ applicant: ApiApplicant }>();
+
+const fullName = computed(
+  () => `${props.applicant.firstName} ${props.applicant.lastName}`
+);
+</script>
