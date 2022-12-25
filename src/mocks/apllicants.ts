75129bc3aa185cae11ec6b58845424e56de1fd3a
Julian Toscani <julian.toscani@rtl.com>
2022-12-25 23:57:00 +0100

    🎨 add composable, remove mock

--- src/mocks/apllicants.ts
+++
@@ -1,9 +0,0 @@
-import type { ApplicantTeaser } from "@/types/ApplicantTeaser.interface";
-
-export const applicants: ApplicantTeaser[] = [
-  {
-    fullName: "Jon Doe",
-    jobDescription: "Developer",
-    image: "https://picsum.photos/300/300",
-  },
-];
